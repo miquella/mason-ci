e1e89071a01f0dff1102bb96dfadb8fd2c59b9df
Mark Severson <markse@instructure.com>
2015-10-15 16:37:57 -0600

    Add job create/get api endpoints

--- mason-master/api.go
+++ mason-master/api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/miquella/mason-ci/datastore"
 	"log"
 	"net/http"
@@ -10,8 +11,10 @@ import (
 func init() {
 	api := Router.PathPrefix("/api").Subrouter()
 
-	jobsApi := api.Path("/jobs").Subrouter()
-	jobsApi.Methods("GET").HandlerFunc(jobsIndexHandler)
+	api.Path("/jobs").Methods("GET").Handler(http.RedirectHandler("jobs/", http.StatusMovedPermanently))
+	api.Path("/jobs/").Methods("GET").HandlerFunc(jobsIndexHandler)
+	api.Path("/jobs/").Methods("POST").HandlerFunc(jobCreateHandler)
+	api.Path("/jobs/{job}").Methods("GET").HandlerFunc(jobGetHandler)
 }
 
 func apiJob(job *datastore.Job) map[string]interface{} {
@@ -44,5 +47,62 @@ func jobsIndexHandler(w http.ResponseWriter, r *http.Request) {
 	err = encoder.Encode(&apiJobs)
 	if err != nil {
 		log.Printf("Failed to encode jobs data: %s", err)
+	}
+}
+
+func jobCreateHandler(w http.ResponseWriter, r *http.Request) {
+	contentType := r.Header["Content-Type"]
+	if len(contentType) != 1 || contentType[0] != "application/json" {
+		http.Error(w, "expected application/json entity", http.StatusBadRequest)
+		return
+	}
+
+	var postedJob *datastore.Job
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&postedJob)
+	if err != nil {
+		log.Printf("Failed to decode job object: %s", err)
+		http.Error(w, "invalid job object provided", http.StatusBadRequest)
+		return
+	}
+
+	job := datastore.Job{
+		Name:        postedJob.Name,
+		Description: postedJob.Description,
+		Config: datastore.JobConfig{
+			Environment: postedJob.Config.Environment,
+			Commands:    postedJob.Config.Commands,
+		},
+	}
+	err = Store.SaveJob(&job)
+	if err != nil {
+		log.Printf("Failed to save job object: %s", err)
+		http.Error(w, "failed to save job object", http.StatusInternalServerError)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(apiJob(&job))
+	if err != nil {
+		log.Printf("Failed to encode new job data: %s", err)
+	}
+}
+
+func jobGetHandler(w http.ResponseWriter, r *http.Request) {
+	jobKey := mux.Vars(r)["job"]
+	job, err := Store.LoadJob(jobKey)
+	if err == datastore.ErrJobNotFound {
+		http.NotFound(w, r)
+		return
+	} else if err != nil {
+		log.Printf("Failed to query job (key: %s): %s", jobKey, err)
+		http.Error(w, "error querying job", http.StatusInternalServerError)
+		return
+	}
+
+	encoder := json.NewEncoder(w)
+	err = encoder.Encode(apiJob(job))
+	if err != nil {
+		log.Printf("Failed to encode job data: %s", err)
 	}
 }
